ac3313827b41874d6b74b329e82b95e511d4f832
Christian Baez <riemanntensor@yahoo.com>
2015-08-12 03:10:42 +0000

    updated for loop iteration

--- sieve.c
+++ sieve.c
@@ -1,24 +1,40 @@
 //Milton Christian Baez
 //8.11.2015 - Christian
 //C programming - Sieve of Erastosthenes Algorithm
+//Ongoing project to keep my coding skills in check
 
 #include <stdio.h>
 
-int intArray[120],n = 0,i;
+int intArray[120],n = 0,i,l;
 
 int main (){
     
+    //statement to detect changes in code - debug
     printf("check statement\n");
     
+    //initial for loop for creating an array with all the numbers from 2 to 120
     for(i=2;i<121;i++){
         
         intArray[n] = i;
+        
         printf("%d\n",intArray[n]);
+        
         n += 1;
         
     }
     
-    printf("%d\n",intArray[3]);
+    for(i=3;i<121;i++){
+        
+        for(l=2;l<121;l++){
+            
+            if(i % l == 0){
+                
+                intArray[i] = 0;
+                
+            }
+        }
+        
+    }
     
     
     
